7330bf8b5be4cbd9bed6595f0eac4ae58ca46634
MustafaJafri <126508338+MustafaJafri@users.noreply.github.com>
2024-07-16 23:30:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+# Additional code
+@app.route('/about')
+def about():
+    return 'About AL Nafi: We are an online e-learning platform.'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}! Welcome to AL Nafi!'
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Thank you, {name}, for submitting the form!'
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            <input type="submit" value="Submit">
+        </form>
+    '''
+
